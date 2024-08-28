d26411f2b1314e0c8e40c0134c07646bd78251b6
fahmi <fahmihanif234@gmail.com>
2024-08-28 21:41:21 +0700

    Menambahkan method PUT

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const {getGoals, setGoals} = require('../controllers/goalControllers')
 
-router.get("/", ( req, res) => {
-  res.status(200).json({ nama: "Fahmii" })
+router.get('/', getGoals)
+router.post('/', setGoals)
+
+router.put('/',(req,res) => {
+    res.json({
+        message: "Menambahkan method Put"
+    })    
 })
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,32 +2,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-// testing API2wss
-// app.use("/", (req, res) => {
-//   res.send("<h1>Halo cok</h1>");
-// })
-
-// app.get("/", (req, res) => {
-//   res.send("halaman home");
-// })
-
-app.post("/", (req, res) => {
-  res.json({
-    nama: "Fahmi",
-    email: "agdgdh@gmail.com",
-    numPhone: "815 99999888"
-  })
-})
-
-// app.use("/api/goals", require("./routes/goalRoutes.js"));
-// app.get('/about', (req, res) => {
-//   res.send('This is the About Page');
-// });
-
-// app.get('/menu', (req, res) => {
-//   res.send('Menu halaman');
-// });
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
